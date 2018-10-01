0672626d70855f79666bdd4a8a9938f7e7e9380a
azamatmurdalov <azamat.murdalov@mail.ru>
2018-10-01 22:32:32 +0300

    Теперь персонаж может стрелять

--- game.py
+++ game.py
@@ -26,6 +26,19 @@ jumpCount = 10
 left = False
 right = False
 animCount = 0
+lastMove = "right"
+
+class snaryad():
+	def __init__(self, x, y, radius, color, facing):
+		self.x = x
+		self.y = y
+		self.radius = radius
+		self.color = color
+		self.facing = facing
+		self.vel = 8 * facing
+
+	def draw(self, win):
+		pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
 def drawWindow():
 	global animCount
@@ -43,9 +56,13 @@ def drawWindow():
 	else:
 		win.blit(playerStand, (x, y))
 
+	for bullet in bullets:
+		bullet.draw(win)
+
 	pygame.display.update()
 
 run = True
+bullets = []
 while run:
 	clock.tick(30)
 
@@ -53,15 +70,32 @@ while run:
 		if event.type == pygame.QUIT:
 			run = False
 
+	for bullet in bullets:
+		if bullet.x < 500 and bullet.x > 0:
+			bullet.x += bullet.vel
+		else:
+			bullets.pop(bullets.index(bullet))
+
 	keys = pygame.key.get_pressed()
+
+	if keys[pygame.K_f]:
+		if lastMove == "right":
+			facing = 1
+		else:
+			facing = -1
+		if len(bullets) < 5:
+			bullets.append(snaryad(round(x + width // 2), round(y + height // 2), 5, (255, 0, 0), facing))
+
 	if keys[pygame.K_LEFT] and x > 5:
 		x -= speed
 		left = True
 		right = False
+		lastMove = "left"
 	elif keys[pygame.K_RIGHT] and x < 500 - width - 5:
 		x += speed
 		left = False
 		right = True
+		lastMove = "right"
 	else:
 		left = False
 		right = False
