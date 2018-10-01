94b64f9119c331ffb0213bea9c4aa678aaf3003b
azamatmurdalov <azamat.murdalov@mail.ru>
2018-10-01 14:33:43 +0300

    Добавлен главный персонаж

--- game.py
+++ game.py
@@ -5,15 +5,49 @@ win = pygame.display.set_mode((500, 500))
 
 pygame.display.set_caption("Cubes Game")
 
+walkRight = [pygame.image.load('right_1.png'), pygame.image.load('right_2.png'), pygame.image.load('right_3.png'), pygame.image.load('right_4.png'), pygame.image.load('right_5.png'), pygame.image.load('right_6.png')]
+
+walkLeft = [pygame.image.load('left_1.png'), pygame.image.load('left_2.png'), pygame.image.load('left_3.png'), pygame.image.load('left_4.png'), pygame.image.load('left_5.png'), pygame.image.load('left_6.png')]
+
+bg = pygame.image.load('bg.jpg')
+playerStand = pygame.image.load('idle.png')
+
+clock = pygame.time.Clock()
+
 x = 50
 y = 425
-width = 40
-height = 60
+width = 60
+height = 71
 speed = 5
+
+isJump = False
+jumpCount = 10
+
+left = False
+right = False
+animCount = 0
+
+def drawWindow():
+	global animCount
+	win.blit(bg, (0, 0))
+
+	if animCount + 1 >= 30:
+		animCount = 0
+
+	if left:
+		win.blit(walkLeft[animCount // 5], (x, y))
+		animCount += 1
+	elif right:
+		win.blit(walkRight[animCount // 5], (x, y))
+		animCount += 1
+	else:
+		win.blit(playerStand, (x, y))
+
+	pygame.display.update()
 
 run = True
 while run:
-	pygame.time.delay(100)
+	clock.tick(30)
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -22,15 +56,30 @@ while run:
 	keys = pygame.key.get_pressed()
 	if keys[pygame.K_LEFT] and x > 5:
 		x -= speed
-	if keys[pygame.K_RIGHT] and x < 500 - width - 5:
+		left = True
+		right = False
+	elif keys[pygame.K_RIGHT] and x < 500 - width - 5:
 		x += speed
-	if keys[pygame.K_UP] and y > 5:
-		y -= speed
-	if keys[pygame.K_DOWN] and y < 500 - height - 15:
-		y += speed
+		left = False
+		right = True
+	else:
+		left = False
+		right = False
+		animCount = 0
+	if not(isJump):
+		if keys[pygame.K_SPACE]:
+			isJump = True
+	else:
+		if jumpCount >= -10:
+			if jumpCount < 0:
+				y += (jumpCount ** 2) / 2
+			else:
+				y -= (jumpCount ** 2) / 2
+			jumpCount -= 1
+		else:
+			isJump = False
+			jump = 10
 
-	win.fill((0,0,0))
-	pygame.draw.rect(win, (0,0,255), (x, y, width, height))
-	pygame.display.update()
+	drawWindow()
 
 pygame.quit()
